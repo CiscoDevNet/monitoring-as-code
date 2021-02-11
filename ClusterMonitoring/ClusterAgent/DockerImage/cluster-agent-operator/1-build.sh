c2093bc65babe9e7f513c4938e7c234d843ef6fa
alex_jov <aleksjovovic@gmail.com>
2021-02-11 18:01:00 +0100

    Cluster agent building an image - add readme; Operator - add readme and build script;

---
+++ ClusterMonitoring/ClusterAgent/DockerImage/cluster-agent-operator/1-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dockerhub_handle=${1}
+image_name=${2}
+image_tag=${3}
+
+if [ "$dockerhub_handle" = "" ]; then
+    dockerhub_handle="alexappd"
+fi
+
+if [ "$image_name" = "" ]; then
+    image_name="cluster-agent-operator
+fi
+
+if [ "$image_tag" = "" ]; then
+    image_tag="latest"
+fi
+
+docker build -t "${dockerhub_handle}/${image_name}:${image_tag}" -f Dockerfile . --no-cache
